fb2fa579ffb3a08496c1172eb63ad72886778693
MT-Joer <719969293@qq.com>
2026-01-02 15:30:56 +0800

    style: 风格同步测试

--- apps/playground/src/views/examples/json-viewer/index.vue
+++ apps/playground/src/views/examples/json-viewer/index.vue
@@ -1,51 +1,45 @@
+<template>
+    <Page description="一个渲染 JSON 结构数据的组件，支持复制、展开等，简单易用"
+          title="Json Viewer">
+        <Card title="默认配置">
+            <JsonViewer :value="json1" />
+        </Card>
+        <Card class="mt-4" title="可复制、默认展开3层、显示边框、事件处理">
+            <JsonViewer boxed
+                        copyable
+                        :expand-depth="3"
+                        :sort="false"
+                        :value="json2"
+                        @copied="handleCopied"
+                        @key-click="handleKeyClick"
+                        @value-click="handleValueClick" />
+        </Card>
+        <Card class="mt-4" title="预览模式">
+            <JsonViewer copyable
+                        preview-mode
+                        :show-array-index="false"
+                        :value="json2" />
+        </Card>
+    </Page>
+</template>
 <script lang="ts" setup>
-import type { JsonViewerAction, JsonViewerValue } from '@vben/common-ui';
+import type { JsonViewerAction, JsonViewerValue } from "@vben/common-ui";
 
-import { JsonViewer, Page } from '@vben/common-ui';
+import { JsonViewer, Page } from "@vben/common-ui";
 
-import { Card, message } from 'ant-design-vue';
+import { Card, message } from "ant-design-vue";
 
-import { json1, json2 } from './data';
+import { json1, json2 } from "./data";
 
 function handleKeyClick(key: string) {
-  message.info(`点击了Key ${key}`);
+    message.info(`点击了Key ${key}`);
 }
 
 function handleValueClick(value: JsonViewerValue) {
-  message.info(`点击了Value ${JSON.stringify(value)}`);
+    message.info(`点击了Value ${JSON.stringify(value)}`);
 }
 
 function handleCopied(_event: JsonViewerAction) {
-  message.success('已复制JSON');
+    message.success("已复制JSON");
 }
 </script>
-<template>
-  <Page
-    title="Json Viewer"
-    description="一个渲染 JSON 结构数据的组件，支持复制、展开等，简单易用"
-  >
-    <Card title="默认配置">
-      <JsonViewer :value="json1" />
-    </Card>
-    <Card title="可复制、默认展开3层、显示边框、事件处理" class="mt-4">
-      <JsonViewer
-        :value="json2"
-        :expand-depth="3"
-        copyable
-        :sort="false"
-        @key-click="handleKeyClick"
-        @value-click="handleValueClick"
-        @copied="handleCopied"
-        boxed
-      />
-    </Card>
-    <Card title="预览模式" class="mt-4">
-      <JsonViewer
-        :value="json2"
-        copyable
-        preview-mode
-        :show-array-index="false"
-      />
-    </Card>
-  </Page>
-</template>
